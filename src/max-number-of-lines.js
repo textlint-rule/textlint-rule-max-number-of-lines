a796a7e0ea6f886e835522634f85607059181518
azu <azuciao@gmail.com>
2017-01-21 17:34:59 +0900

    refactor: migrate textlint-scripts

--- src/max-number-of-lines.js
+++ src/max-number-of-lines.js
@@ -1,18 +1,18 @@
 // LICENSE : MIT
 "use strict";
-import ObjectAssign from "object-assign";
+const ObjectAssign = require("object-assign");
 const defaultOptions = {max: 300};
-export default function (context, options = defaultOptions) {
+module.exports = function(context, options = defaultOptions) {
     options = ObjectAssign({}, defaultOptions, options);
-    let max = options.max;
-    let {Syntax, RuleError, report, getSource} = context;
+    const max = options.max;
+    const {Syntax, RuleError, report, getSource} = context;
     return {
         [Syntax.Document](node) {
-            var text = getSource(node);
-            var len = text.split("\n").length;
+            const text = getSource(node);
+            const len = text.split("\n").length;
             if (len > max) {
                 report(node, new RuleError(`Document is too long(number of lines: ${len}).`));
             }
         }
     }
-}
+};
--- test/max-number-of-lines-test.js
+++ test/max-number-of-lines-test.js
@@ -1,31 +1,30 @@
-import {textlint} from "textlint";
-import rule from "../src/max-number-of-lines";
-import path from "path";
-import assert from "power-assert";
-describe("max-number-of-lines", function () {
-    afterEach(function () {
-        textlint.resetRules();
-    });
-    beforeEach(function () {
-        textlint.setupRules({
-            "max-number-of-lines": rule
-        }, {
-            "max-number-of-lines": {
-                "max": 42
-            }
-        });
-    });
-    it("should report error", function () {
-        var filePath = path.join(__dirname, "/fixtures/long.md");
-        var result = textlint.lintFile(filePath);
-        assert(result.filePath === filePath);
-        assert(result.messages.length > 0);
-        assert.equal(result.messages[0].ruleId, "max-number-of-lines");
-    });
-    it("should not report error", function () {
-        var filePath = path.join(__dirname, "/fixtures/short.md");
-        var result = textlint.lintFile(filePath);
-        assert(result.filePath === filePath);
-        assert(result.messages.length === 0);
-    });
+const TextLintTester = require("textlint-tester");
+const tester = new TextLintTester();
+const path = require("path");
+const fs = require("fs");
+
+// rule
+const rule = require("../src/max-number-of-lines");
+// ruleName, rule, { valid, invalid }
+tester.run("rule", rule, {
+    valid: [
+        // no problem
+        "text"
+    ],
+    invalid: [
+        // single match
+        {
+            text: fs.readFileSync(path.join(__dirname, "fixtures", "long.md"), "utf-8"),
+            options: {
+                max: 42
+            },
+            errors: [
+                {
+                    message: "Document is too long(number of lines: 50).",
+                    line: 1,
+                    column: 1
+                }
+            ]
+        }
+    ]
 });
